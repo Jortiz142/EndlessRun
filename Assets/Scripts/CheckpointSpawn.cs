c0da99f63c682a443d0a0b90152a27e114823833
Jortiz142 <45494715+Jortiz142@users.noreply.github.com>
2020-04-18 19:40:19 -0400

    Checkpoint fixed

--- Assets/Scripts/CheckpointSpawn.cs
+++ Assets/Scripts/CheckpointSpawn.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class CheckpointSpawn : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
-    private Vector3 obstacleSpawnPos = new Vector3(25, 0, 0);
-    private float obstacleStartDelay = 2;
-    private float obstacleRepeatRate = 2;
+    private float checkpointStartDelay = 5;
+    private float checkpointRepeatRate = 5;
+    public GameObject checkpointPrefab;
+    private Vector3 checkpointSpawnPos = new Vector3(25, 6, 0);
     private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", obstacleStartDelay, obstacleRepeatRate);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnCheckpoint", checkpointStartDelay, checkpointRepeatRate);
     }
-
-    void SpawnObstacle()
+    void SpawnCheckpoint()
     {
         if (playerControllerScript.gameOver == false && playerControllerScript.finished == false)
         {
-            Instantiate(obstaclePrefab, obstacleSpawnPos, obstaclePrefab.transform.rotation);
+            Instantiate(checkpointPrefab, checkpointSpawnPos, checkpointPrefab.transform.rotation);
+            checkpointRepeatRate++;
         }
     }
 
